1d0be9b3f7c667a5e29b0ade1095eee83dc6f8f2
ShivamBytes18 <su9555181178@gmail.com>
2026-07-31 23:18:36 +0530

    Deployment Issue

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -125,8 +125,8 @@ const loginUser = asynchandler(async (req,res)=>{
 
    const options = {
      httpOnly:true,
-     secure:false,
-     sameSite:"lax"
+     secure:true,
+     sameSite:"none"
    }
 
    return res.status(200)
