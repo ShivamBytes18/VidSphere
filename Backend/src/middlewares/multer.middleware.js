00d6f1be6c139efd21e854e8ed6432caf14a2bfa
ShivamBytes18 <su9555181178@gmail.com>
2026-07-31 22:56:02 +0530

    Deploy

--- Backend/src/middlewares/multer.middleware.js
+++ Backend/src/middlewares/multer.middleware.js
@@ -1,22 +1,69 @@
+// // import multer from "multer";
+
+// // const storage = multer.diskStorage({
+// //   destination: function (req, file, cb) {
+// //     cb(null, "./public/temp")
+// //   },
+// //   filename: function (req, file, cb) {
+// //     cb(null, file.originalname)
+// //   }
+// // })
+
+// //  export const upload = multer({ storage,    })
+
 // import multer from "multer";
+// import path from "path";
 
 // const storage = multer.diskStorage({
 //   destination: function (req, file, cb) {
-//     cb(null, "./public/temp")
+//     cb(null, "./public/temp");
 //   },
 //   filename: function (req, file, cb) {
-//     cb(null, file.originalname)
-//   }
-// })
+//     const uniqueName =
+//       Date.now() + "-" + Math.round(Math.random() * 1e9);
 
-//  export const upload = multer({ storage,    })
+//     cb(null, uniqueName + path.extname(file.originalname));
+//   }
+// });
+
+// const fileFilter = (req, file, cb) => {
+//   const allowedTypes = [
+//     "image/jpeg",
+//     "image/png",
+//     "image/jpg",
+//     "video/mp4",
+//     "video/mkv"
+//   ];
+
+//   if (allowedTypes.includes(file.mimetype)) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Invalid file type"), false);
+//   }
+// };
+
+// export const upload = multer({
+//   storage,
+//   fileFilter,
+//   limits: {
+//     fileSize: 500  * 1024 * 1024 // 50MB
+//   }
+// });
+
 
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadPath = "./public/temp";
+
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
     const uniqueName =
@@ -26,26 +73,10 @@ const storage = multer.diskStorage({
   }
 });
 
-const fileFilter = (req, file, cb) => {
-  const allowedTypes = [
-    "image/jpeg",
-    "image/png",
-    "image/jpg",
-    "video/mp4",
-    "video/mkv"
-  ];
-
-  if (allowedTypes.includes(file.mimetype)) {
-    cb(null, true);
-  } else {
-    cb(new Error("Invalid file type"), false);
-  }
-};
-
 export const upload = multer({
   storage,
   fileFilter,
   limits: {
-    fileSize: 500  * 1024 * 1024 // 50MB
+    fileSize: 500 * 1024 * 1024
   }
 });
